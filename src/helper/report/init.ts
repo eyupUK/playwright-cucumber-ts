391238c169f0d43d14738a398de3c91b55c20d92
eyup <eyuptozcu@gmail.com>
2025-04-19 16:59:05 +0100

    Refactor report generation to use dynamic browser info and update report details for consistency

--- src/helper/report/init.ts
+++ src/helper/report/init.ts
@@ -2,7 +2,6 @@ const fs = require("fs-extra");
 try {
     fs.ensureDir("test-results");
     fs.emptyDir("test-results");
-
 } catch (error) {
     console.log("Folder not created! " + error);
 }
--- src/helper/report/report.ts
+++ src/helper/report/report.ts
@@ -1,26 +1,35 @@
-const report = require("multiple-cucumber-html-reporter");
+import report = require("multiple-cucumber-html-reporter");
+import os = require("node:os");
+
+const browserInfo = require("../../../browser-info.json");
 
 report.generate({
     jsonDir: "test-results",
     reportPath: "test-results/reports/",
-    reportName: "Legacy Apps Test Report",
-    pageTitle: "Legacy Apps Test Report",
-    displayDuration: false,
+    reportName: "My App Test Report",
+    pageTitle: "My App Test Report",
+    displayDuration: true,
+    durationInMS: true,
+    openReportInBrowser: true,
+    removeExistingJsonReportFile: true,
+    displayReportTime: true,
+    displayReportTimeInSeconds: true,
+    useCDN: true,
     metadata: {
         browser: {
-            name: "chrome",
-            version: "112",
+            name: browserInfo.name,
+            version: browserInfo.version,
         },
-        device: "ETO",
+        device: "Macbook Pro",
         platform: {
-            name: "Mac OS ",
-            version: "14.5",
+            name: os.platform(),
+            version: os.version(),
         },
     },
     customData: {
         title: "Test Info",
         data: [
-            { label: "Project", value: "New CRM" },
+            { label: "Project", value: "My App" },
             { label: "Release", value: "1.1.1" },
             { label: "Cycle", value: "Smoke-1" }
         ],
